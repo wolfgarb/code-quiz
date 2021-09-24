738a9c96a8a20be37997c420021e6fb6c599359e
wolfgarb <88980251+wolfgarb@users.noreply.github.com>
2021-09-24 10:44:00 -0400

    Timer is alivegit add -A

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,54 @@
-function countdown() {
-    var counter = 60;
-    $("#countdown").show(counter)
-}
+// const questions = [
+//     {
+//     question:"what is 2 + 2",
+//     answers:["4", "5", "12", "0"],
+//     }
+// ]
+
+const questions = [
+    {
+    question:"what is 2 + 2",
+    answers:["4", "5", "12", "0"],
+    }
+]
 
 
-// const questions = [{
-//     question:"",
-//     answers:[""],
-// }]
-
-
-function startQuiz() {
-    countdown();
-}
-
-$("#start-btn").on("click", function() {
-    $(this).hide("slide");
-    $("#quiz-description").hide("slide");
-    startQuiz();
-});
-
+// function will end quiz and allow user to enter high score
 function endGame() {
     console.log("Game Over");
 }
+
+function countdown() {
+    var timer, counter = 60;
+    $("#counter").text(counter);
+    timer = setInterval(function() {
+        counter--;
+        $("#counter").text(counter);
+        if (counter === 0) {
+            clearInterval(timer);
+            endGame();
+        }
+    }, 1000);
+}
+
+
+
+
+
+// when the quiz begins, the countdown will begin.
+function startQuiz() {
+    console.log("let the countdown begin");
+    $("#quiz-content").removeClass("invisible")
+}
+
+// get start button by id. when its clicked, it will be hidden, along with the quiz description. then the quiz will start.
+$("#start-btn").on("click", function() {
+    $(this).hide("slide");
+    $("#quiz-description").hide("slide");
+
+    startQuiz();
+    countdown();
+});
+
 
 
